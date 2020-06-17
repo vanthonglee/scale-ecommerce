149dc02c6ee2bce4384456f4bc74f284a480f61c
Văn Thông Lê <thonglevan131@gmail.com>
2020-06-17 12:42:24 +0700

    removed unused codes

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Directory from '../../components/directory/directory.component';
 
-import './homepage.style.scss'
 import { HomePageContainer } from './homepage.style';
 
 const HomePage = () => (
