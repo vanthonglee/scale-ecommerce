7c23b61ee6c25dc34e2510b968305785d737fd28
Văn Thông Lê <thonglevan131@gmail.com>
2020-07-08 16:59:23 +0700

    updating our code to be pwa compliant, added serviceWorker, https, updating manifest

---
+++ client/build/precache-manifest.b768883d93f863fabd85c27bc32d46b4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d8e13e81403e99a101423bce01cd3ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "258f16a832154cd4e433",
+    "url": "/static/css/4.961f0f56.chunk.css"
+  },
+  {
+    "revision": "9cab3e8a0bf7f17852b3",
+    "url": "/static/css/5.a811e489.chunk.css"
+  },
+  {
+    "revision": "4fa9b11521e6185979c3",
+    "url": "/static/css/main.56345a2a.chunk.css"
+  },
+  {
+    "revision": "c454963d2ccfa88914b5",
+    "url": "/static/js/2.a94181c6.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.a94181c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bddb495009291d52021d",
+    "url": "/static/js/3.5390f31b.chunk.js"
+  },
+  {
+    "revision": "258f16a832154cd4e433",
+    "url": "/static/js/4.ac4840d8.chunk.js"
+  },
+  {
+    "revision": "9cab3e8a0bf7f17852b3",
+    "url": "/static/js/5.8f253a68.chunk.js"
+  },
+  {
+    "revision": "bc899b659f4ac86c38d9",
+    "url": "/static/js/6.b2c3e5f9.chunk.js"
+  },
+  {
+    "revision": "44a949427927661a6edf",
+    "url": "/static/js/7.0bda8e64.chunk.js"
+  },
+  {
+    "revision": "568a8c35fb2724656287",
+    "url": "/static/js/8.2325577e.chunk.js"
+  },
+  {
+    "revision": "c66f03050cade07b35c9",
+    "url": "/static/js/9.8ba6c1b9.chunk.js"
+  },
+  {
+    "revision": "4fa9b11521e6185979c3",
+    "url": "/static/js/main.b4b19c80.chunk.js"
+  },
+  {
+    "revision": "0ec00a98c8b5e7e78008",
+    "url": "/static/js/runtime-main.7b7a35a4.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Profiler } from 'react';
 
 import Directory from '../../components/directory/directory.component';
 
@@ -6,7 +6,15 @@ import { HomePageContainer } from './homepage.style';
 
 const HomePage = () => (
     <HomePageContainer>
-        <Directory />
+        <Profiler id="Directory"  onRender={(id,phase,actualDuration) =>{
+            console.log({
+                id,
+                phase,
+                actualDuration
+            });
+        }}>
+            <Directory />
+        </Profiler>
     </HomePageContainer>
 );
 
